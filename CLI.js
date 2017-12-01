16a870aec85aa8a6b9ba51ca62a2272b1565c560
Jason Lin <jlin0402@gmail.com>
2017-11-30 19:34:49 -0800

    create weather search function

--- CLI.js
+++ CLI.js
@@ -2,8 +2,23 @@
 var weather = require('weather-js');
 var axios = require('axios');
 var fs = require('fs');
-
+var userType = process.argv[2];
 // create user and admin on process.argv[2]
+var userSearch = function(name, location) {
+    this.name = name;
+    this.location = location; 
+    this.date = Date.now();
+
+    this.getWeather = function() {
+        weather.find({search: this.location, defreeType: "C"}, function(err, result){
+            if(err) console.log(err);
+
+            console.log(JSON.stringify(result, null, 2));
+        });
+    }
+}
+
+
 
 // create method for admin to request list of past searches
 
