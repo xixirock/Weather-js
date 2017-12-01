c941bf0de30d742474c2442e7c47eb7a827fbc1e
Jason Lin <jlin0402@gmail.com>
2017-11-30 19:07:54 -0800

    typed out all required

---
+++ CLI.js
@@ -0,0 +1,10 @@
+// install required
+var weather = require('weather-js');
+var axios = require('axios');
+var fs = require('fs');
+
+// create user and admin on process.argv[2]
+
+// create method for admin to request list of past searches
+
+// implement logic for user search
